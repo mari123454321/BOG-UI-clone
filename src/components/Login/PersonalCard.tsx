556d732b088937e405534bdeaaf4044ff269ef6c
Mariam S. <sikhuashvilimariami@gmail.com>
2025-11-02 21:35:38 +0400

    fixed personal card

--- src/components/Login/PersonalCard.tsx
+++ src/components/Login/PersonalCard.tsx
@@ -29,34 +29,40 @@ export default function PersonalCard() {
     const currentLanguage = translations[language]
     return (
         <div className="card-container">
-            <section className="personal-card">
-                <div className="personal-card-title">
+            <section className="auth-card">
+                <div className="auth-card-title">
                     <h2 >{currentLanguage.title}</h2>
                 </div>
 
-                <div className="personal-card-form-container">
+                <div className="auth-card-form-container">
                     <form>
-                        <div className="personal-card-username">
-                            <input type="text" name="username" placeholder={currentLanguage.usernamePlaceholder} required className="login-input" />
-                            <label>
-                                <input type="checkbox" name="saveUsername" value={"save username"} />
-                                {currentLanguage.saveUsername}
-                            </label>
+                        <div className="auth-card-auth-methods">
+                            <div className="auth-card-input-container">
+                                <div className="auth-card-username">
+                                    <input type="text" name="username" placeholder={currentLanguage.usernamePlaceholder} required className="login-input" />
+                                    <label className="auth-save-username">
+                                        <input type="checkbox" name="saveUsername" value={"save username"} className="checkbox" />
+                                        {currentLanguage.saveUsername}
+                                    </label>
+                                </div>
+                                <div className="auth-card-password">
+                                    <input type="password" name="password" placeholder={currentLanguage.passwordPlaceholder} required className="login-input" />
+                                    <a href=""><span  className="auth-forgot-password">{currentLanguage.fotgot}</span></a> {/*droebiti*/}
+                                </div>
+                            </div>
+{/* 
+                            <div className="auth-card-qr">
+                                <img src={qr} alt="QR" />
+                                <p>{currentLanguage.QRtext}</p>
+                            </div> */}
                         </div>
-                        <div className="personal-card-password">
-                            <input type="password" name="password" placeholder={currentLanguage.passwordPlaceholder} required className="login-input" />
-                            <a href="">{currentLanguage.fotgot}</a> {/*droebiti*/}
-                        </div>
-                        {/* <div className="personal-card-qr">
-                            <img src={qr} alt="QR" />
-                            <p>{currentLanguage.QRtext}</p>
-                        </div> */}
-                        <div className="personal-card-buttons">
+
+                        <div className="auth-card-buttons">
                             <button className="button1">{currentLanguage.button1}</button>
                             <button className="button2">{currentLanguage.button2}</button>
-                        
+
                         </div>
-                        
+
                     </form>
                 </div>
 
--- src/layouts/LoginPageLayout.tsx
+++ src/layouts/LoginPageLayout.tsx
@@ -24,8 +24,8 @@ export default function LoginPageLayput({ logoTitle, logoText }: Props) {
     const pathname = (location.pathname as LoginPath) ?? "/personal"
     const classNameLogo = pathname == "/personal" ? "auth-BOG-logo auth-BOG-personal-logo" : pathname == "/manager" ? "auth-BOG-logo auth-BOG-manager-logo" : "auth-BOG-logo auth-BOG-business-logo"
     return (
-        <div className="login-personal">
-            <div className="login-personal-left">
+        <div className="login">
+            <div className="login-left">
                 <AuthHeader />
                 <div className={classNameLogo}>
                     <img src="BOGlogo.png" alt="BOG logo" className="auth-BOGlogo" />
@@ -34,14 +34,14 @@ export default function LoginPageLayput({ logoTitle, logoText }: Props) {
                         {logoText && <span>{logoText}</span>}
                     </div>
                 </div>
-                <main className="PersonalCardContainer">
+                <main className="authCardContainer">
                     {cards[pathname]}
                 </main>
                 <div className="auth-footer">
                     <Footer/>   
                 </div>
             </div>
-            <div className="login-personal-right">
+            <div className="login-right">
                 <img src="https://ramad.bog.ge/s3/sso/images/c90514a2-f523-44f0-9e70-11960cb75582.jpg" alt="მობაილ ბანკის გათამაშება" />
             </div>
 
